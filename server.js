245c3c5b3779bdd10844983faec8fe9e0807b52a
Charletontech <121673088+Charletontech@users.noreply.github.com>
2024-03-27 18:54:21 -0700

    Update server.js first edit

--- server.js
+++ server.js
@@ -41,6 +41,37 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/get-all-products', (req, res) => {
+    const products = [
+        {
+            id: 1,
+            Name: 'Nike Air',
+            img: 'img/Screenshot_20240324-140030.jpg',
+            price: 'N 10,000',
+            category: 'Shoes',
+            qty: 1
+        },
+        {
+            id: 2,
+            Name: 'Headphone Pro',
+            img: 'img/Screenshot_20240324-135928.jpg',
+            price: 'N 15,000',
+            category: 'Gadgets',
+            qty: 1
+        },
+        {
+            id: 3,
+            Name: 'Headphone Max',
+            img: 'img/Screenshot_20240324-135928.jpg',
+            price: 'N 20,000',
+            category: 'Gadgets',
+            qty: 1
+        }
+    ];
+    
+    res.json(products)
+})
+
 
 app.listen(5000, () => {
     console.log('Cranshaw at port 5000...');
